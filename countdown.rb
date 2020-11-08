3dc1448178fe25906faaeaa357737572927fcf50
John Million <jpmillion@comcast.net>
2020-11-08 12:38:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,7 +8,6 @@ def countdown(int)
   return "HAPPY NEW YEAR!"
 end
 
-countdown(5)
 
 def countdown_with_sleep(int)
   while int >= 1 do
@@ -19,4 +18,3 @@ def countdown_with_sleep(int)
   return "HAPPY NEW YEAR!"
 end
 
-countdown_with_sleep(5)
